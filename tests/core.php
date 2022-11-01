251061c0798f048dbe5c5c16882efc73c82ed7ad
Marijn Ophorst <marijn@monomelodies.nl>
2022-11-01 15:49:23 +0100

    update/fix tests

--- tests/core.php
+++ tests/core.php
@@ -1,40 +1,31 @@
 <?php
 
-use Ornament\Demo\{ CoreModel, CoreModel74, DecoratedModel, DecoratedModel74, SubtractOne };
+use Ornament\Demo\{ CoreModel, DecoratedModel, SubtractOne };
 use Gentry\Gentry\Wrapper;
 
 /**
  * Tests for core Ornament functionality.
  */
 return function () : Generator {
-    /**
-     * Core models should have only the basic functionality: expose properties
-     * via magic getters and setters but not private ones.
-     */
+    /** Core models should have only the basic functionality: expose properties via magic getters and setters but not private ones. */
     yield function () : void {
-        $model = Wrapper::createObject((float)phpversion() >= 7.4 ? CoreModel74::class : CoreModel::class);
+        $model = Wrapper::createObject(CoreModel::class);
         assert(isset($model->id));
         assert($model->id == 1);
         assert(!isset($model->invisible));
     };
 
-    /**
-     * Models can successfully register and apply decorations. The second
-     * field is decorated using constructor arguments. We can also retrieve a
-     * virtual field via a getter method.
-     */
+    /** Models can successfully register and apply decorations. */
     yield function () : void {
-        $model = Wrapper::createObject((float)phpversion() >= 7.4 ? DecoratedModel74::class : DecoratedModel::class);
+        $model = Wrapper::createObject(DecoratedModel::class);
         $model->set('field', 2);
         assert((int)"{$model->field}" === 1);
         assert($model->virtual_property === "1");
     };
 
-    /**
-     * If we try to access a private property, an Error is thrown.
-     */
+    /** If we try to access a private property, an Error is thrown. */
     yield function () : void {
-        $model = Wrapper::createObject((float)phpversion() >= 7.4 ? CoreModel74::class : CoreModel::class);
+        $model = Wrapper::createObject(CoreModel::class);
         $e = null;
         try {
             $foo = $model->invisible;
@@ -43,11 +34,9 @@ return function () : Generator {
         assert($e instanceof Error);
     };
 
-    /**
-     * If we try to modify a protected property, an Error is thrown.
-     */
+    /** If we try to modify a protected property, an Error is thrown. */
     yield function () : void {
-        $model = Wrapper::createObject((float)phpversion() >= 7.4 ? CoreModel74::class : CoreModel::class);
+        $model = Wrapper::createObject(CoreModel::class);
         $e = null;
         try {
             $model->id = 2;
