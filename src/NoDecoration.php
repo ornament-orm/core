52c2136d88ee5799c9a4638a2126f75dff902158
Marijn Ophorst <marijn@sensimedia.nl>
2024-12-12 16:19:01 +0100

    explain better

--- src/NoDecoration.php
+++ src/NoDecoration.php
@@ -4,6 +4,10 @@ namespace Ornament\Core;
 
 use Attribute;
 
+/**
+ * If, for whatever reason, a property should explicitly _not_ be dedorated,
+ * you may use the `Ornament\Core\NoDecoration` attribute.
+ */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class NoDecoration
 {
