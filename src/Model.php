cb87126a69088ccf77256e28644bed7f23488896
Marijn Ophorst <marijn@sensimedia.nl>
2024-12-13 15:55:07 +0100

    remove deprecated stuff; handle readonly properties correctly

--- src/Model.php
+++ src/Model.php
@@ -11,12 +11,14 @@ use Error;
 trait Model
 {
     /**
-     * @var callable
-     */
-    private static $arrayToModelTransformer;
-
-    /**
-     * Constructor.
+     * Constructor. Takes an iterable `$input` (e.g. a row from a database) as
+     * argument. Key/value pairs are assumed.
+     *
+     * Each known public property, if available in `$input`, is populated with
+     * the corresponding value, cast to the correct (decorated) type. Note that
+     * `readonly` properties may only be set from the actual class defining them
+     * (not child or parent classes). Ornament ignores these; it is up the
+     * implementor to make sure all parent constructors are called.
      *
      * @param iterable|null $input
      * @return void
@@ -27,31 +29,19 @@ trait Model
             $cache = Helpers::getModelPropertyDecorations($this);
             foreach ($cache['properties'] as $field => $annotations) {
                 if (array_key_exists($field, $input)) {
-                    $this->$field = Helpers::ornamentalize($this, $field, $input[$field]);
+                    try {
+                        $this->$field = Helpers::ornamentalize($this, $field, $input[$field]);
+                    } catch (Error $e) {
+                        // Probably a readonly property defined in a parent
+                        // class. Implementations with inherited classes
+                        // _should_ take care to invoke all parent constructors,
+                        // so as to avoid uninitialized readonly properties.
+                    }
                 }
             }
         }
-        Repository::setInitial($this);
-    }
 
-    /**
-     * Initialize the iterable-to-model transformer for this class. The default
-     * is to pass a hash of key/value pairs to the constructor.
-     *
-     * @param callable|null $transformer
-     * @return void
-     */
-    public static function initTransformer(callable $transformer = null) : void
-    {
-        if (!isset(self::$arrayToModelTransformer)) {
-            self::$arrayToModelTransformer = function (iterable $data) : object {
-                $class = __CLASS__;
-                return new $class($data);
-            };
-        }
-        if (isset($transformer)) {
-            self::$arrayToModelTransformer = $transformer;
-        }
+        Repository::setInitial($this);
     }
 
     /**
@@ -73,57 +63,6 @@ trait Model
             throw new Error("Only non-static properties can be `set` ($field in ".get_class($this).")");
         }
         $this->$field = Helpers::ornamentalize($this, $field, $value);
-    }
-
-    /**
-     * Overloaded getter. All public and protected properties on a model are
-     * exposed this way, unless explicitly marker with the NoDecoration
-     * attribute. Non-public properties are read-only.
-     *
-     * @param string $prop Name of the property.
-     * @return mixed The property's value.
-     * @throws Error if the property is unknown.
-     */
-    public function __get(string $prop)
-    {
-        $cache = Helpers::getModelPropertyDecorations($this);
-        if (isset($cache['methods'][$prop])) {
-            return $this->{$cache['methods'][$prop]}();
-        }
-        try {
-            $reflection = new ReflectionProperty($this, $prop);
-        } catch (ReflectionException $e) {
-            throw new Error("Tried to get non-existing property $prop on ".get_class($this));
-        }
-        if ($reflection->isPublic() && !$reflection->isStatic()) {
-            return $this->$prop;
-        } else {
-            throw new Error("Tried to get private or abstract property $prop on ".get_class($this));
-        }
-    }
-    
-    /**
-     * Check if a property is defined. Note that this will return true for
-     * protected properties.
-     *
-     * @param string $prop The property to check.
-     * @return boolean True if set, otherwise false.
-     */
-    public function __isset(string $prop) : bool
-    {
-        $cache = Helpers::getModelPropertyDecorations($this);
-        if (isset($cache['methods'][$prop])) {
-            return true;
-        }
-        try {
-            $reflection = new ReflectionProperty($this, $prop);
-        } catch (ReflectionException $e) {
-            return false;
-        }
-        if ($reflection->isPublic()) {
-            return isset($this->$prop);
-        }
-        return false;
     }
 }
 
